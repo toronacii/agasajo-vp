14ac9d818bcfd04798f09844ba7f248ce3637cfa
toronacii <toronacii@gmail.com>
2019-12-11 17:06:03 -0300

    Adding libraries

--- delegates-web/main.js
+++ delegates-web/main.js
@@ -1,10 +1,35 @@
-getFiles().then(initialize)
+$(function () {
+    getFiles().then(initialize)
 
-function getFiles() {
-    return fetch('./files.json?' + Math.random())
-        .then(res => res.json())
-}
+    function getFiles() {
+        return fetch('./files.json?' + Math.random())
+            .then(res => res.json())
+    }
 
-function initialize(files) {
-    console.log(files);
-}
+    function initialize(files) {
+        // console.log(files);
+        var $container = $('#container');
+
+        const html = files.map(file => $(`<div class="item"><img data-src="https://drive.google.com/uc?export=view&id=${file.id}" /></div>`));
+
+        $container.append(html);
+
+        $('.item img').Lazy({
+            scrollDirection: 'vertical',
+            effect: 'fadeIn',
+            visibleOnly: true,
+            onError: function (element) {
+                console.log('error loading ' + element.data('src'));
+            }
+        });
+
+        // $container.imagesLoaded(function () {
+        //     $container.masonry({
+        //         itemSelector: '.item',
+        //         columnWidth: function (containerWidth) {
+        //             return containerWidth / 12;
+        //         }
+        //     });
+        // });
+    }
+});
